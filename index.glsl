70680c64f4a0eb8fad6d5412622573f206b4c00c
Tezirg <jean.grizet@gmail.com>
2017-07-07 11:43:09 +0100

    Macro improvement
    Update the PI definition and usage.
    Renamed to M_PI for a more C-like/GLSL style.
    More significant number for precision
    Protects the macro against double definition

--- index.glsl
+++ index.glsl
@@ -1,4 +1,6 @@
-#define PI 3.14159265
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
 
 float orenNayarDiffuse(
   vec3 lightDirection,
@@ -18,7 +20,7 @@ float orenNayarDiffuse(
   float A = 1.0 + sigma2 * (albedo / (sigma2 + 0.13) + 0.5 / (sigma2 + 0.33));
   float B = 0.45 * sigma2 / (sigma2 + 0.09);
 
-  return albedo * max(0.0, NdotL) * (A + B * s / t) / PI;
+  return albedo * max(0.0, NdotL) * (A + B * s / t) / M_PI;
 }
 
 #pragma glslify: export(orenNayarDiffuse)
